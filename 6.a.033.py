54f8c91a8239483709b70872b4326d316a81f76b
sarwesh57 <sarweshvaran59@gmail.com>
2025-08-06 21:33:26 +0530

    Create 6.a.033.py

---
+++ 6.a.033.py
@@ -0,0 +1,24 @@
+OPERATORS = set(['+','-','*','/','(',')','^'])
+PRIORITY ={'+':1,'-':1,'*':2,'/':2,'^':3}
+def infix__to__postfix(expression):
+    stack =[]
+    output =''
+    for ch in expression:
+        if ch not in OPERATORS:
+            output+= ch
+        elif ch=='(':
+            stack.append('(')
+        elif ch==')':
+            while stack and stack[-1]!='(':
+                output+=stack.pop()
+            stack.pop()
+        else:
+            while stack and stack[-1]!='('and PRIORITY[ch]<=PRIORITY[stack[-1]]:
+                output+=stack.pop()
+            stack.append(ch)
+    while stack:
+        output+=stack.pop()
+    return output
+expression=input('enter infix expression')
+print('infix expression:',expression)
+print('postfix expression:',infix__to__postfix(expression))
